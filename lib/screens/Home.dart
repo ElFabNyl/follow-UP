2dacf310e359d1139b91118333963dce40b175aa
nyll <elfabnyl@gmail.com>
2021-06-21 15:28:30 +0100

    some refactorings

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
-// import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 import 'package:followup/brain/controller.dart';
 import 'package:followup/models/Convert.dart';
-import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:followup/widgets/Widgets.dart';
+import 'package:followup/dataGrid/ConvertDataSource.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -47,234 +45,20 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         child: Icon(Icons.person_add),
       ),
       drawer: Container(
-        child: _drawer(),
+        child: Widgets.drawer(),
         width: 200.0,
         padding: EdgeInsets.zero,
       ),
       backgroundColor: Colors.white,
-      appBar: _buildAppBar(),
+      appBar: Widgets.buildAppBar(),
       body: SafeArea(
         child: Column(
           children: [
-            _appBarBottomSection(context, controller),
-            _contentGrid(convertDataSource),
+            Widgets.appBarBottomSection(context, controller),
+            Widgets.contentGrid(convertDataSource),
           ],
         ),
       ),
-    );
-  }
-}
-
-//creating the  appBar
-
-AppBar _buildAppBar() {
-  return AppBar(
-    backgroundColor: Colors.grey,
-    title: Text("Young Converts"),
-    actions: [
-      Padding(
-        padding: const EdgeInsets.only(right: 15.0),
-        child: Icon(
-          Icons.notifications,
-          color: Colors.white,
-        ),
-      )
-    ],
-  );
-}
-
-//building the drawer
-Drawer _drawer() {
-  return Drawer(
-    child: ListView(
-      padding: EdgeInsets.zero,
-      children: [
-        UserAccountsDrawerHeader(
-          accountName: Text('Kamtou boris'),
-          accountEmail: Text("email@gmail.com"),
-          currentAccountPicture: CircleAvatar(
-            backgroundColor: Colors.white,
-            child: Icon(
-              Icons.person,
-              color: Colors.grey,
-            ),
-          ),
-          decoration: BoxDecoration(
-            color: Colors.grey,
-          ),
-        ),
-        ListTile(
-          leading: Icon(Icons.person_add),
-          title: Text("Add Convert"),
-        ),
-        ListTile(
-          leading: Icon(Icons.list),
-          title: Text("Converts Added"),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            ListTile(
-              leading: Icon(Icons.logout_rounded),
-              title: Text("Logout"),
-            ),
-          ],
-        ),
-      ],
-    ),
-  );
-}
-
-//more content
-
-_contentGrid(convertDataSource) {
-  return Padding(
-    padding: const EdgeInsets.only(top: 15.0),
-    child: SfDataGrid(
-      source: convertDataSource,
-      columns: <GridColumn>[
-        GridTextColumn(
-          columnName: 'Name',
-          label: Container(
-            padding: EdgeInsets.all(16.0),
-            alignment: Alignment.centerRight,
-            child: Text(
-              'Name',
-            ),
-          ),
-        ),
-        GridTextColumn(
-          columnName: 'Phone',
-          label: Container(
-            padding: EdgeInsets.all(16.0),
-            alignment: Alignment.centerLeft,
-            child: Text('Phone'),
-          ),
-        ),
-        GridTextColumn(
-          columnName: 'Location',
-          width: 120,
-          label: Container(
-            padding: EdgeInsets.all(16.0),
-            alignment: Alignment.centerLeft,
-            child: Text('Location'),
-          ),
-        ),
-        GridTextColumn(
-          columnName: 'F.Name',
-          label: Container(
-            padding: EdgeInsets.all(16.0),
-            alignment: Alignment.centerRight,
-            child: Text('F.Name'),
-          ),
-        ),
-        GridTextColumn(
-          columnName: 'F.phone',
-          label: Container(
-            padding: EdgeInsets.all(16.0),
-            alignment: Alignment.centerRight,
-            child: Text('F.phone'),
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-//buildind the appBarBottom section here
-
-Container _appBarBottomSection(context, AnimationController controller) {
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 50.0),
-    width: MediaQuery.of(context).size.width,
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.only(
-        bottomLeft: Radius.circular(50),
-        bottomRight: Radius.circular(50),
-      ),
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: TextLiquidFill(
-            boxHeight: 70.0,
-            boxBackgroundColor: Colors.white,
-            waveColor: Colors.black,
-            text: 'Our Goal',
-            textStyle: GoogleFonts.pacifico(
-              textStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w900),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 8.0,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              '${controller.value.toInt()}',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.w500,
-                  height: 1),
-            ),
-            Text(
-              'disciples',
-              style: TextStyle(
-                  textBaseline: TextBaseline.alphabetic,
-                  color: Colors.grey,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w900,
-                  height: 1),
-            ),
-          ],
-        )
-      ],
-    ),
-  );
-}
-
-class ConvertDataSource extends DataGridSource {
-  ConvertDataSource({required List<Convert> convertis}) {
-    _convertis = convertis
-        .map<DataGridRow>((e) => DataGridRow(cells: [
-              DataGridCell<String>(columnName: 'name', value: e.name),
-              DataGridCell<String>(columnName: 'number', value: e.number),
-              DataGridCell<String>(columnName: 'location', value: e.location),
-              DataGridCell<String>(
-                  columnName: 'follower Name', value: e.followerName),
-              DataGridCell<String>(
-                  columnName: 'follower Phone', value: e.followerphone),
-            ]))
-        .toList();
-  }
-  List<DataGridRow> _convertis = [];
-  @override
-  List<DataGridRow> get rows => _convertis;
-
-  @override
-  DataGridRowAdapter? buildRow(DataGridRow row) {
-    return DataGridRowAdapter(
-      cells: row.getCells().map<Widget>((dataGridCell) {
-        return Container(
-          alignment: (dataGridCell.columnName == 'name' ||
-                  dataGridCell.columnName == 'follower Phone')
-              ? Alignment.centerRight
-              : Alignment.centerLeft,
-          padding: EdgeInsets.all(16.0),
-          child: Text(dataGridCell.value.toString()),
-        );
-      }).toList(),
     );
   }
 }
---
+++ lib/widgets/Widgets.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class Widgets {
+  //more content
+
+  static contentGrid(convertDataSource) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15.0),
+      child: SfDataGrid(
+        source: convertDataSource,
+        columns: <GridColumn>[
+          GridTextColumn(
+            columnName: 'Name',
+            label: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.centerRight,
+              child: Text(
+                'Name',
+              ),
+            ),
+          ),
+          GridTextColumn(
+            columnName: 'Phone',
+            label: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.centerLeft,
+              child: Text('Phone'),
+            ),
+          ),
+          GridTextColumn(
+            columnName: 'Location',
+            width: 120,
+            label: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.centerLeft,
+              child: Text('Location'),
+            ),
+          ),
+          GridTextColumn(
+            columnName: 'F.Name',
+            label: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.centerRight,
+              child: Text('F.Name'),
+            ),
+          ),
+          GridTextColumn(
+            columnName: 'F.phone',
+            label: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.centerRight,
+              child: Text('F.phone'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //buildind the appBarBottom section here
+
+  static Container appBarBottomSection(
+      context, AnimationController controller) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 50.0),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: TextLiquidFill(
+              boxHeight: 70.0,
+              boxBackgroundColor: Colors.white,
+              waveColor: Colors.black,
+              text: 'Our Goal',
+              textStyle: GoogleFonts.pacifico(
+                textStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w900),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '${controller.value.toInt()}',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w500,
+                    height: 1),
+              ),
+              Text(
+                'disciples',
+                style: TextStyle(
+                    textBaseline: TextBaseline.alphabetic,
+                    color: Colors.grey,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w900,
+                    height: 1),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  //creating the  appBar
+
+  static AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.grey,
+      title: Text("Young Converts"),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 15.0),
+          child: Icon(
+            Icons.notifications,
+            color: Colors.white,
+          ),
+        )
+      ],
+    );
+  }
+
+//building the drawer
+  static Drawer drawer() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('Kamtou boris'),
+            accountEmail: Text("email@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.person,
+                color: Colors.grey,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.person_add),
+            title: Text("Add Convert"),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.list),
+            title: Text("Converts Added"),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ListTile(
+                onTap: () {},
+                leading: Icon(Icons.logout_rounded),
+                title: Text("Logout"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
